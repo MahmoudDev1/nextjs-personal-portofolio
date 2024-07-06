66c7970f74f41c841f8ad02d93bc05688e1b4c83
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-07-06 12:38:53 +0300

    added expiry date to locale cookie

--- actions.ts
+++ actions.ts
@@ -6,6 +6,9 @@ export async function changeLang(lang: string, locale: string) {
   if (lang == locale) {
     return;
   }
-
-  cookies().set("locale", lang);
+  const currentDate = new Date()
+  const expireDate = new Date(currentDate).setFullYear(currentDate.getFullYear() + 1)
+  cookies().set("locale", lang, {
+    expires: expireDate
+  });
 }
