51d39d1fe3cd4ea2a2608417260b981bf533e313
Mahmoud <mahmoudfarghly519@gmail.com>
2024-08-11 18:32:35 +0300

    added custom offline page + new configurations

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
+  cacheStartUrl: false,
+  fallbacks: {
+    document: "/offline"
+  },
+  reloadOnOnline: true
 });
 
 /** @type {import('next').NextConfig} */
